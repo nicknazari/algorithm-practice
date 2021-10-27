15dce13566e6def0320950846ab805b79b9d82eb
nicknazari <nhn2915@gmail.com>
2021-10-26 21:38:53 -0400

    started huffman and lzw

--- lzw/LZW.java
+++ lzw/LZW.java
@@ -1,6 +1,10 @@
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
+
+import javax.xml.namespace.QName;
 
 // Nick Nazari
 // practice implementation of LZW compression
@@ -17,31 +21,55 @@ class LZW {
 		
 		// parsing our input file to a string so that is can be broken in to bits
 		StringBuilder input = new StringBuilder();
+		ArrayList<Integer> inputList = new ArrayList<Integer>();
 		Scanner s = new Scanner(new FileInputStream(filename));
 		while (s.hasNext()) {
 			char[] line = s.next().toCharArray();
 			for (char c: line) {
-				input.append((int) c);
+				input.append((int) c + " ") ;
 			}
 		}
 		s.close(); 
+
+		HashMap<Character, Integer> codebook = new HashMap<Character, Integer>();
+		// put all possible ascii codes in codebook
+		for (int i = 0; i < 256; i++) {
+			codebook.put((char) i, i);
+		}
+
+		for (Character c : codebook.keySet()) {
+			System.out.println(c);
+		}
+		
+		output = compress(input.toString());
 
 		System.out.println("Input: " + input);
 		System.out.println("Operation: " + operation);
 		System.out.println("Output: " + output);
 	}
 
-	private String compress(String ascii) {
+	public static String matchLongestPrefix(ArrayList<String> in) {
+		StringBuilder longest = new StringBuilder();
+		int pos = 0;
+
+		StringBuilder prefix = new StringBuilder();
+		for (String s : in) {
+			
+		}
+
+		longest.append(prefix + in.get(pos + 1));
+		return longest.toString();
+	}
+
+	public static String compress(String ascii) {
 		String out = null;
 		return out;
 
 	}
 
-	private String expand(String ascii) {
+	public static String expand(String ascii) {
 		String out = null;
 		return out;
 	}
 
 }
-
-
