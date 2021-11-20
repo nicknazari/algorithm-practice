a6a62be2b1f0a7d90c345709d8c87837b86ee152
nicknazari <nhn2915@gmail.com>
2021-11-20 18:42:12 -0500

    added minheap

--- heap/MinHeap.java
+++ heap/MinHeap.java
@@ -1,3 +1,20 @@
-class MinHeap {
+class MinHeap<T> {
+    T[] arr = new T[5];
 
+    public static void main(String[] args) {
+
+    }
+
+    private void add() {
+
+    }
+
+    private void remove() {
+
+    }
+
+    private int find() {
+        int pos = 0;
+        return pos;
+    }
 }
