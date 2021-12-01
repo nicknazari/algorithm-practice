09e610b72b351f2ed422249fd9bbe25161431d4a
nicknazari <nhn2915@gmail.com>
2021-12-01 18:07:14 -0500

    added graphs and heap

--- graphs/weighted_tree_uf.java
+++ graphs/weighted_tree_uf.java
@@ -1,17 +1,38 @@
-public UF( int n) {
-   count = n;
-   id = new int[n];
-   sz = new int[n];
-   for (int i = 0; i < n; i ++) { id[i] = i; sz[i] = 1; }
-}
-public int find ( int p) {
-   while (p != id[p]) p = id[p];
-   return p;
-}
-public void union (int p, int q) {
-   int i = find (p), j = find (q);
-   if (i == j) return ;
-   if (sz[i] < sz[j]) { id[i] = j; sz[j] += sz[i]; }
-   else { id[j] = i; sz[i] += sz[j]; }
-   count --;
+public class weighted_tree_uf {
+   static int n = 8;
+   static int count = n;
+   static int[] id = new int[n];
+   static int[] sz = new int[n];
+
+   public static int find(int p) {
+      while (p != id[p]) p = id[p];
+      return p;
+   }
+
+   public static void union(int p, int q) {
+      int i = find(p);
+      int j = find(q);
+      if (i == j) return;
+      if (sz[i] < sz[j]) { id[i] = j; sz[j] += sz[i]; }
+      else { id[j] = i; sz[i] += sz[j]; }
+      count --;
+   }
+   public static void main(String[] args) {
+
+      for (int i = 0; i < n; i ++) {
+         id[i] = i; sz[i] = 1; 
+      }
+      union(2,0);
+      union(7,4);
+      union(1,2);
+      union(1, 3); 
+      union(4, 5);
+      union(5, 7);
+      union(5, 3);
+
+      System.out.println(find(0));
+
+
+   
+   }
 }
